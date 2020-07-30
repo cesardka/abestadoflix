feadbfb364808a7ce84260e9f9ea9fa0d52fb6e1
Cesar H <cesardka@hotmail.com>
2020-07-30 03:26:28 -0300

    Dia 3 - Controle de estados em formulário
    Foram complementadas as páginas de cadastro de Categoria e Vídeos para
    que funcione um formulário gerenciando os campos através do useState do
    React
    
    Cada uma das páginas espera pelos seguintes campos a serem preenchidos:
    
    - Categoria
    
    * Nome (text)
    * Descrição (textarea)
    * Cor       (text)
    
    - Video
    
    * Nome (text)
    * Link do Youtube (text)
    * Descrição       (textarea)
    
    No futuro próximo ao invés de preencher esses dados de forma estática
    numa lista na página, vamos fazer com que preencha de verdade esses
    dados através de uma chamada de hook (ou qualquer outra tecnologia que
    seja instruída ao longo do curso)
    
    Lembrando que - pelo amor de Deus - esses formulários precisam de um
    carinho urgente para que fiquem pelo menos bonitinhos e interessantes
    
    Tem muito chão pela frente!!

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -1,11 +1,85 @@
-import React       from 'react';
-import { Link }    from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState } from 'react';
+import { Link }            from 'react-router-dom';
+import PageDefault         from '../../../components/PageDefault';
+import FormField           from '../../../components/FormField';
 
 function CadastroCategoria() {
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '',
+    }
+
+    const [categorias, setCategorias] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
+
+    function setValue(chave, valor) {
+        // chave: nome, descricao, bla, bli
+        setValues({
+            ...values,
+            [chave]: valor, // nome: 'valor'
+        })
+    }
+
+    function handleChange(infosDoEvento) {
+        setValue(
+            infosDoEvento.target.getAttribute('name'),
+            infosDoEvento.target.value
+        );
+    }
+
     return (
         <PageDefault>
-            <h1>Nova Categoria</h1>
+            <h1>Nova Categoria {values.nome} </h1>
+
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                setCategorias([
+                    ...categorias,
+                    values
+                ]);
+
+                setValues(valoresIniciais)
+            }}>
+
+                <FormField
+                label="Nome da Categoria"
+                type="text"
+                name="nome"
+                value={values.nome}
+                onChange={handleChange}
+                />
+
+                <FormField
+                label="Descrição:"
+                type="????"
+                name="descricao"
+                value={values.descricao}
+                onChange={handleChange}
+                />
+
+                <FormField
+                    label="Cor"
+                    type="color"
+                    name="cor"
+                    value={values.cor}
+                    onChange={handleChange}
+                />
+
+                <button>
+                Cadastrar
+                </button>
+            </form>
+
+            <ul>
+                {categorias.map((categoria, indice) => {
+                return (
+                    <li key={`${categoria}${indice}`}>
+                    {categoria.nome}
+                    </li>
+                )
+                })}
+            </ul>
 
             <Link to="/video/novo">
                 Cadastrar Vídeo
--- src/pages/Cadastro/Video/index.js
+++ src/pages/Cadastro/Video/index.js
@@ -1,11 +1,83 @@
-import React       from 'react';
-import { Link }    from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState } from 'react';
+import { Link }            from 'react-router-dom';
+import PageDefault         from '../../../components/PageDefault';
+import FormField           from '../../../components/FormField';
 
 function CadastroVideo() {
+    // Valores iniciais de FormField
+    const valoresIniciais = {
+        nome: '',
+        link: '',
+        descricao: '',
+    }
+
+    // Estados
+    const [videos, setVideos] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
+
+    // Função que altera valor de um atributo
+    function setValue(chave, valor) {
+        setValues({
+            ...values,
+            [chave]: valor, // posição [chave] dentro de values recebe valor
+        });
+    }
+
+    // Handler de submit do formulário
+    function handleSubmit(infoEvento) {
+        infoEvento.preventDefault();
+        setVideos([
+            ...videos,
+            values
+        ]);
+
+        setValues(valoresIniciais); // Reseta os valores dos inputs todos
+    }
+
+    // Handler de alteração de input
+    function handleChange(infoEvento) {
+        setValue(
+            infoEvento.target.getAttribute('name'),
+            infoEvento.target.value
+        );
+    }
+
     return (
         <PageDefault>
-            <h1>Nova Vídeo</h1>
+            <h1>Nova Vídeo: {values.nome}</h1>
+
+            <form onSubmit={handleSubmit}>
+                {/* Nome do vídeo      TEXT     */}
+                <FormField
+                    label="Nome"
+                    name="nome"
+                    type="text"
+                    value={values.nome}
+                    onChange={handleChange}
+                />
+
+                {/* Link do vídeo      TEXT     */}
+                <FormField
+                    label="Link Youtube"
+                    name="link"
+                    type="text"
+                    value={values.link}
+                    onChange={handleChange}
+                />
+
+                {/* Descrição do vídeo TEXTAREA */}
+                <FormField
+                    label="Descrição"
+                    name="descricao"
+                    type="textarea"
+                    value={values.descricao}
+                    onChange={handleChange}
+                />
+
+                <button>
+                    Cadastrar
+                </button>
+            </form>
 
             <Link to="/categoria/novo">
                 Cadastrar Categoria
